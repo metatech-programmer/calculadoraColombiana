082701f82dd74f561a4181157ff2b428460a0c3f
allen2564 <97856988+allen2564@users.noreply.github.com>
2022-12-18 11:55:32 -0500

    pesos

--- main.js
+++ main.js
@@ -1,12 +1,70 @@
-//Hallar el cuatro por mil
 
-const cuatroPorMil = (valor) => (valor / 1000) * 4;
+/**
+ * Función que convierte un número a su representación
+ * monetaria en JavaScript, por ejemplo, el número
+ * 123456 se convierte en $ 123, 456.00
+*/
+const aMoneda = (numero, opciones) => {
+    // Valores por defecto
+    opciones = opciones || {};
+    opciones.simbolo = opciones.simbolo || "$";
+    opciones.separadorDecimal = opciones.separadorDecimal || ".";
+    opciones.separadorMiles = opciones.separadorMiles || ",";
+    opciones.numeroDeDecimales = opciones.numeroDeDecimales >= 0 ? opciones.numeroDeDecimales : 2;
+    opciones.posicionSimbolo = opciones.posicionSimbolo || "i";
+    const CIFRAS_MILES = 3;
+    
+    // Redondear y convertir a cadena
+    let numeroComoCadena = numero.toFixed(opciones.numeroDeDecimales);
+    
+    // Comenzar desde la izquierda del separador o desde el final de la cadena si no se proporciona
+    let posicionDelSeparador = numeroComoCadena.indexOf(opciones.separadorDecimal);
+    if (posicionDelSeparador === -1) posicionDelSeparador = numeroComoCadena.length;
+    let formateadoSinDecimales = "", indice = posicionDelSeparador;
+    // Ir cortando desde la derecha de 3 en 3, y concatenar en una nueva cadena
+    while (indice >= 0) {
+        let limiteInferior = indice - CIFRAS_MILES;
+        // Agregar separador si cortamos más de 3
+        formateadoSinDecimales = (limiteInferior > 0 ? opciones.separadorMiles : "") + numeroComoCadena.substring(limiteInferior, indice) + formateadoSinDecimales;
+        indice -= CIFRAS_MILES;
+    }
+    let formateadoSinSimbolo = `${formateadoSinDecimales}${numeroComoCadena.substr(posicionDelSeparador, opciones.numeroDeDecimales + 1)}`;
+    return opciones.posicionSimbolo === "i" ? opciones.simbolo + formateadoSinSimbolo : formateadoSinSimbolo + opciones.simbolo;
+};
 
-
-function calcularcuatroPorMil() {
-    let valor = document.getElementById('valor');
-    let value = parseFloat(valor.value);
-
-    const result = `El valor que debera pagar adiconal por utilizar $ ${valor.value} pesos es: \n $ ${cuatroPorMil(value)} pesos.`;
-    document.getElementById('resultado').innerText = result;
+// Probar
+const opcionesPesosColombianos = {
+    numeroDeDecimales: 2,
+    separadorDecimal: ".",
+    separadorMiles: ",",
+    simbolo: "$ ", // Con un espacio, ya que la función no agrega espacios
+    posicionSimbolo: "i", // i = izquierda, d = derecha
 }
+// ,
+//     opcionesDolares = {
+    //         numeroDeDecimales: 2,
+    //         separadorDecimal: ".",
+    //         separadorMiles: ",",
+    //         simbolo: " USD", // Con un espacio, ya que la función no agrega espacios
+    //         posicionSimbolo: "d", // i = izquierda, d = derecha
+    //     };
+    
+    const numeros = [1.652, 12, 123, 1234, 12345.335, 123456, 1234567, 12345678, 123456789, 1234567890, 12345678901, 123456789012, 586.512];
+    console.log("Probando con pesos...");
+    numeros.forEach(numero => console.log(`Con ${numero} sale ${aMoneda(numero, opcionesPesosMexicanos)}`));
+    console.log("Probando con dólares...");
+    numeros.forEach(numero => console.log(`Con ${numero} sale ${aMoneda(numero, opcionesDolares)}`));
+
+
+    //Hallar el cuatro por mil
+    
+    const cuatroPorMil = (valor) => (valor / 1000) * 4;
+    
+    
+    function calcularcuatroPorMil() {
+        let valor = document.getElementById('valor');
+        let value = parseFloat(valor.value);
+    
+        const result = `El valor que debera pagar adiconal por utilizar $ ${aMoneda(valor.value,opcionesPesosColombianos)} pesos es: \n $ ${aMoneda(cuatroPorMil(value),opcionesPesosColombianos)} pesos.`;
+        document.getElementById('resultado').innerText = result;
+    }
